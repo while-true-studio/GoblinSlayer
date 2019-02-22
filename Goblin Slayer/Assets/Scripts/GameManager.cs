49eca7c4c3ed30b43d970bd86ab43ba5698140b0
Usuario Local <anasca01@ucm.es>
2019-02-22 13:12:37 +0100

    added reset

---
+++ Goblin Slayer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public int vida;
+    public int index;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+	}
+    /// <summary>
+  
+
+    /// <summary>
+    /// Reseteara la escena si  la vida del jugador es 0 o inferior 
+    /// </summary>
+    /// <param name="index">
+    /// Indica qué escena se va a reiniciar
+    /// </param
+    public void ResetScene(int index)
+    { 
+        SceneManager.LoadScene(index);
+    }
+   
+}
