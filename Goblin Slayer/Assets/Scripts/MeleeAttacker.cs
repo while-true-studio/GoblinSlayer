1a0fb32453146a26c781ffe52986c8a7475b045d
Kyra <kyradiscord@gmail.com>
2019-02-27 12:38:03 +0100

    feat: Added FindGameObjects and MakeAttack

--- Goblin Slayer/Assets/Scripts/MeleeAttacker.cs
+++ Goblin Slayer/Assets/Scripts/MeleeAttacker.cs
@@ -18,7 +18,41 @@ public class MeleeAttacker : MonoBehaviour
 	void Update()
     {
         // TODO: Remove this before merging
-        FindObjects(Vector2.right);
+        MakeAttack(new Vector2(1.0f, 1.0f));
+    }
+
+    /// <summary>
+    /// Make an attack on all game objects between the game object that has this component
+    /// and the vector it is aiming to inside a range that has no obstacle between both.
+    /// </summary>
+    /// <param name="normalVector">The normal vector the game object is aiming to.</param>
+    public void MakeAttack(Vector2 normalVector)
+    {
+        GameObject[] gameObjects = FindGameObjects(normalVector);
+        // TODO: Filter gameobjects by its component "Attackable", when
+        // it becomes available.
+    }
+
+    /// <summary>
+    /// Find all game objects between the game object that has this component and the vector
+    /// it is aiming to inside a range, filtering the game object itself and all the objects
+    /// between the first wall (if found) until the end.
+    /// </summary>
+    /// <param name="normalVector">The normal vector the game object is aiming to.</param>
+    /// <returns>An array of GameObject.</returns>
+    public GameObject[] FindGameObjects(Vector2 normalVector)
+    {
+        // Get all raycast hits between the game object and the target vector (normal vector * range).
+        RaycastHit2D[] raycastHits = FindRaycastHits(normalVector);
+        
+        // Filter all raycast hits until it finds a wall.
+        int max = 0;
+        while (max < raycastHits.Length && raycastHits[max].collider.tag != "Wall") max++;
+
+        // Make an array of GameObjects mapping the raycast hits array.
+        GameObject[] gameObjects = new GameObject[max];
+        for (int i = 1; i < max; i++) gameObjects[i - 1] = raycastHits[i].collider.gameObject;
+        return gameObjects;
     }
 
     /// <summary>
@@ -27,7 +61,7 @@ public class MeleeAttacker : MonoBehaviour
     /// </summary>
     /// <param name="normalVector">The normal vector the game object is aiming to.</param>
     /// <returns>An array of RaycastHit2D.</returns>
-    RaycastHit2D[] FindObjects(Vector2 normalVector)
+    public RaycastHit2D[] FindRaycastHits(Vector2 normalVector)
     {
         Vector2 fromPosition = transform.position;
         Vector2 toPosition = normalVector * range;
