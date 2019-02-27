2784e0ed6f5aad30a01e20143205ed231c3df0dc
Borja <borcano@ucm.es>
2019-02-27 13:52:15 +0100

    Added attack and die dynamic

---
+++ Goblin Slayer/Assets/Script/Attackable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Health))]
+[RequireComponent(typeof(IDead))]
+public class Attackable : MonoBehaviour
+{
+    private Health health;
+    private IDead dead;
+    private void Start()
+    {
+        health = GetComponent<Health>();
+        dead = GetComponent<IDead>();
+    }
+    public void OnAttack(int damage)
+    {
+        health.ReciveDamage(damage);
+        if (!health.isAlive)
+            dead.OnDead();
+            
+    }
+
+
+}
---
+++ Goblin Slayer/Assets/Script/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health: MonoBehaviour
+{
+    public float health { get; private set; }
+    public float maxHealth { get; private set; }
+    public bool isAlive { get; private set; }
+    private void Start()
+    {
+        isAlive = true;
+    }
+    public void ReciveDamage(float damage)
+    {
+        health -= damage;
+        if (health < 0)
+        {
+            health = 0;
+            isAlive = false;
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health += amount;
+        if (health > maxHealth) health = maxHealth;
+    }
+
+}
