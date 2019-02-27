7d9cfef07d0c7b605f8cb12557407fc4c4a0eda6
Kyra <kyradiscord@gmail.com>
2019-02-27 11:57:06 +0100

    misc: Make FindObjects take a normal vector, added documentation

--- Goblin Slayer/Assets/Scripts/MeleeAttacker.cs
+++ Goblin Slayer/Assets/Scripts/MeleeAttacker.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MeleeAttacker : MonoBehaviour
 {
+    /// <summary>
+    /// The melee attack range for this component.
+    /// </summary>
     public float range = 3;
 
     // Use this for initialization
@@ -14,19 +17,27 @@ public class MeleeAttacker : MonoBehaviour
 	// Update is called once per frame
 	void Update()
     {
-        FindObject();
+        // TODO: Remove this before merging
+        FindObjects(Vector2.right);
     }
 
-    RaycastHit2D[] FindObject()
+    /// <summary>
+    /// Find all colliders between the game object that has this component and the
+    /// vector it is aiming to inside a range.
+    /// </summary>
+    /// <param name="normalVector">The normal vector the game object is aiming to.</param>
+    /// <returns>An array of RaycastHit2D</returns>
+    RaycastHit2D[] FindObjects(Vector2 normalVector)
     {
         Vector2 fromPosition = transform.position;
-        Vector2 toPosition = Vector2.right * range;
+        Vector2 toPosition = normalVector * range;
         RaycastHit2D[] raycastHits = Physics2D.RaycastAll(fromPosition, toPosition, range);
+
+        // TODO: Remove the following two lines before merging.
         // Checks for more than one hits, since the player itself is always included
         bool hit = raycastHits.Length > 1;
-
         Debug.DrawRay(fromPosition, toPosition, hit ? Color.green : Color.red);
-        return hit ? raycastHits : null;
+        return raycastHits;
     }
 
 }
