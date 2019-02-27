7b96999ee77f389b407f2a8984e3b793761d469c
Amaro Blest <ablest@ucm.es>
2019-02-27 12:47:21 +0100

    Added shooter and projectile

---
+++ Goblin Slayer/Assets/Scripts/Projectile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float projectileSpeed = 1.0f;
+    private Rigidbody2D rb;
+    private Vector2 shootDirection;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Set the direction to shoot
+    /// </summary>
+    /// <param name="direction"></param>
+    public void SetDirection(Vector2 direction)
+    {
+        this.shootDirection = direction;
+    }
+    
+    /// <summary>
+    /// Activate a initial velocity 
+    /// </summary>
+    public void ShootYourSelf()
+    {
+        rb.velocity = shootDirection * projectileSpeed;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Attackable target = collision.gameObject.GetComponent<Attackable>();
+        if (target && collision.tag != transform.tag)
+        {
+            target.ReciveDmg(damage);
+        }
+        Destroy(gameObject);
+    }
+
+}
---
+++ Goblin Slayer/Assets/Scripts/Shooter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    public Projectile projectile;
+    public bool isPlayer, meleeDistance;
+
+
+
+    /// <summary>
+    /// Crea el proyectil en la posición del padre
+    /// </summary>
+    private Projectile CreateProjectile()
+    {
+        return Instantiate(projectile, transform.position, Quaternion.identity, gameObject.transform);
+    }
+
+
+    /// <summary>
+    /// Create a proyectile in transform position
+    /// </summary>
+    /// <param name="direction"></param>
+    public void Shoot(Vector2 direction)
+    {
+        Projectile p = CreateProjectile();
+        p.SetDirection(direction);
+        p.ShootYourSelf();
+    }
+
+    public bool IsPlayer()
+    {
+        return isPlayer;
+    }
+
+}
