7eb17ec081968af3addd8b55ff248372096a06a9
Kyra <kyradiscord@gmail.com>
2019-03-01 11:41:57 +0100

    fix: Broken filter for gameobjects

--- Goblin Slayer/Assets/Scripts/MeleeAttacker.cs
+++ Goblin Slayer/Assets/Scripts/MeleeAttacker.cs
@@ -18,7 +18,7 @@ public class MeleeAttacker : MonoBehaviour
 	void Update()
     {
         // TODO: Remove this before merging
-        MakeAttack(new Vector2(1.0f, 1.0f));
+        MakeAttack(Vector2.right);
     }
 
     /// <summary>
@@ -28,9 +28,12 @@ public class MeleeAttacker : MonoBehaviour
     /// <param name="normalVector">The normal vector the game object is aiming to.</param>
     public void MakeAttack(Vector2 normalVector)
     {
-        GameObject[] gameObjects = FindGameObjects(normalVector);
-        // TODO: Filter gameobjects by its component "Attackable", when
-        // it becomes available.
+        var gameObjects = FindGameObjects(normalVector);
+
+#if UNITY_EDITOR
+        bool hit = gameObjects.Count > 0;
+        Debug.DrawRay(transform.position, normalVector * range, hit ? Color.green : Color.red);
+#endif
     }
 
     /// <summary>
@@ -40,18 +43,20 @@ public class MeleeAttacker : MonoBehaviour
     /// </summary>
     /// <param name="normalVector">The normal vector the game object is aiming to.</param>
     /// <returns>An array of GameObject.</returns>
-    public GameObject[] FindGameObjects(Vector2 normalVector)
+    /// <!-- TODO: Change GameObject to Attackable -->
+    public List<GameObject> FindGameObjects(Vector2 normalVector)
     {
         // Get all raycast hits between the game object and the target vector (normal vector * range).
         RaycastHit2D[] raycastHits = FindRaycastHits(normalVector);
-        
-        // Filter all raycast hits until it finds a wall.
-        int max = 0;
-        while (max < raycastHits.Length && raycastHits[max].collider.tag != "Wall") max++;
+        List<GameObject> gameObjects = new List<GameObject>();
 
-        // Make an array of GameObjects mapping the raycast hits array.
-        GameObject[] gameObjects = new GameObject[max];
-        for (int i = 1; i < max; i++) gameObjects[i - 1] = raycastHits[i].collider.gameObject;
+        for (int i = 1; i < raycastHits.Length; i++)
+        {
+            var raycastHit = raycastHits[i];
+            if (raycastHit.collider.tag == "Wall") break;
+            if (raycastHit.collider.gameObject) gameObjects.Add(raycastHit.collider.gameObject);
+        }
+
         return gameObjects;
     }
 
@@ -65,13 +70,7 @@ public class MeleeAttacker : MonoBehaviour
     {
         Vector2 fromPosition = transform.position;
         Vector2 toPosition = normalVector * range;
-        RaycastHit2D[] raycastHits = Physics2D.RaycastAll(fromPosition, toPosition, range);
-
-        // TODO: Remove the following two lines before merging.
-        // Checks for more than one hits, since the player itself is always included
-        bool hit = raycastHits.Length > 1;
-        Debug.DrawRay(fromPosition, toPosition, hit ? Color.green : Color.red);
-        return raycastHits;
+        return Physics2D.RaycastAll(fromPosition, toPosition, range);
     }
 
 }
