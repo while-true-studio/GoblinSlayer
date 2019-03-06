8105bd9a3c356b9529176bc01763f2679badafd1
OskarFreestyle <47836840+OskarFreestyle@users.noreply.github.com>
2019-03-06 10:50:46 +0100

    There is an unknown error

--- Goblin Slayer/Assets/Scripts/Scripts HUD/ManaController.cs
+++ Goblin Slayer/Assets/Scripts/Scripts HUD/ManaController.cs
@@ -6,6 +6,16 @@ using UnityEngine.UI;
 
 public class ManaController : MonoBehaviour
 {
+    //There is an error
+    /*
+     * NullReferenceException: Object reference not set to an instance of an object
+        ManaController.Start () (at Assets/Scripts/Scripts HUD/ManaController.cs:17)
+
+        NullReferenceException: Object reference not set to an instance of an object
+        ManaController.Update () (at Assets/Scripts/Scripts HUD/ManaController.cs:25)
+        
+         The manaBar works like we hope, but It stil having an unkwon error*/
+         
     Mana mana;
     float regenMana, testMana;//This testMana will be change to fireMana, aquaMana and windMana. 
     Scrollbar manaBar;
