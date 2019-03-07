ce8ceb3ba62ab8d829e7753362df83b791423277
Stiven Arias <StArias@ucm.es>
2019-03-07 18:07:39 +0100

    Game Over Screen ready to use

---
+++ Goblin Slayer/Assets/Scripts/TestChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TestChangeScene : MonoBehaviour {
+    
+    public void ChangeScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
