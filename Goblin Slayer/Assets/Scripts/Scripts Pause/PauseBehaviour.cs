2e6d2bbd6ed4bc376aad51b6909cb0e7f2f5beae
Stiven <starias@ucm.es>
2019-03-19 20:52:36 +0100

    Añadido método para congelar cuando se pausa el juego

--- Goblin Slayer/Assets/Scripts/Scripts Pause/PauseBehaviour.cs
+++ Goblin Slayer/Assets/Scripts/Scripts Pause/PauseBehaviour.cs
@@ -12,7 +12,7 @@ public class PauseBehaviour : MonoBehaviour
     //The order of the HUD's children in Hierarchy
     //is the most important thing to use this script.
     //It will activate or desactivate the GOs in function of an strict order.
-
+    bool pauseMode;
     private void Start()
     {
         DesactivePause();
@@ -22,17 +22,24 @@ public class PauseBehaviour : MonoBehaviour
     {
         transform.GetChild(0).gameObject.SetActive(true);
         transform.GetChild(1).gameObject.SetActive(false);
-        transform.GetChild(2).gameObject.SetActive(false);
-        transform.GetChild(3).gameObject.SetActive(false);
+        //transform.GetChild(2).gameObject.SetActive(false);
+        //transform.GetChild(3).gameObject.SetActive(false);
+        pauseMode = true;
     }
 
     public void DesactivePause()
     {
         transform.GetChild(0).gameObject.SetActive(false);
         transform.GetChild(1).gameObject.SetActive(true);
-        transform.GetChild(2).gameObject.SetActive(true);
-        transform.GetChild(3).gameObject.SetActive(true);
+        //transform.GetChild(2).gameObject.SetActive(true);
+        //transform.GetChild(3).gameObject.SetActive(true);
+        pauseMode = false;
     }
 
+    public void Freeze()
+    {
+        if (pauseMode) Time.timeScale = 0;
+        else Time.timeScale = 1;
 
+    }
 }
---
+++ Goblin Slayer/Assets/Scripts/Test Scripts/TestMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour {
+
+    public float speed = 2.0f;
+    public float limiteLateral;
+    PauseBehaviour pause;
+    private void Start()
+    {
+        pause = GameObject.Find("HUD").GetComponent<PauseBehaviour>();
+    }
+
+    void Update ()
+    {
+        transform.Translate(speed * Time.deltaTime, 0f, 0);
+        pause.Freeze();
+        if (transform.position.x >= limiteLateral || transform.position.x < -limiteLateral)
+            speed = -speed;
+	}
+}
