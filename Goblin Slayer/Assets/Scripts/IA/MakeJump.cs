ae054fead284598b795f0d768aa20e0fe288b52e
Borja Cano <borcano@ucm.es>
2019-03-20 11:12:59 +0100

    MakeJump component implemented

---
+++ Goblin Slayer/Assets/Scripts/IA/MakeJump.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class MakeJump : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Follower_AI follower = collision.GetComponent<Follower_AI>();
+        //TODO: This problably needs a revision to make sure the jumping works
+        if (follower)
+            follower.MakeMeJump();
+        
+    }
+}
