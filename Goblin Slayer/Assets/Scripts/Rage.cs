26d403b2355eba4094d1325fb3497ee91e0b4ed2
Stiven <starias@ucm.es>
2019-03-20 18:38:10 +0100

    Completado el script Rage.cs, añadiendo los estados de la rabia al aumentar ésta

--- Goblin Slayer/Assets/Scripts/Rage.cs
+++ Goblin Slayer/Assets/Scripts/Rage.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Rage : MonoBehaviour {
@@ -7,12 +8,33 @@ public class Rage : MonoBehaviour {
     public int rageMax; //The maximun rage of the player
     public float currentRage;
     public float decreaseRageRate; //The rate to decrease the rage by time
-   
+    public float percentage;
+    public float velBoost;
+    public float dmgBoost;
+
+    public enum State {NORMAL = 0, MASACRE = 1, SLAYER = 2};
+
+    [Serializable]
+    public struct RageBoost
+    {
+        public float vel;
+        public float dmg;
+    }
+
+    [SerializeField]
+    public State rageState;
+    public RageBoost [] rageBoost;
 
     void Update()
     {
-        if (currentRage < 0) currentRage = 0;
-        currentRage -= decreaseRageRate * Time.deltaTime;        
+        percentage = (currentRage * 100) / rageMax;
+
+        if (currentRage < 0) { currentRage = 0; }
+        currentRage -= decreaseRageRate * Time.deltaTime;
+
+        if (percentage < 40) rageState = State.NORMAL;
+        else if (percentage >= 40 && percentage < 90) rageState = State.MASACRE;
+        else if (percentage >= 90 && percentage <= 100) rageState = State.SLAYER;
     }
 
     /// <summary>
@@ -22,5 +44,5 @@ public class Rage : MonoBehaviour {
     {
         currentRage += plusRage; 
         if (currentRage > rageMax) currentRage = rageMax;
-    }   
+    }
 }
