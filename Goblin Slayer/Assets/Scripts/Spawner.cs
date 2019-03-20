1e94ad5628d45db8b465aba0ed20703c7302c6d4
Kyra <kyradiscord@gmail.com>
2019-03-20 11:03:33 +0100

    feat: Spawner and SpawnerTrigger

---
+++ Goblin Slayer/Assets/Scripts/Spawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    /// <summary>
+    /// The GameObject prefab this Spawner should instantiate.
+    /// </summary>
+    public GameObject unit;
+
+    /// <summary>
+    /// The bucket for this spawner, this equals to the amount
+    /// of `unit` clones this will create.
+    /// </summary>
+    public short remaining = 20;
+
+    /// <summary>
+    /// The time interval in seconds for the spawner, an higher
+    /// amount means more time between spawns.
+    /// </summary>
+    public float spawnInterval = 3.5f;
+    
+    /// <summary>
+    /// Start the spawning coroutine.
+    /// </summary>
+    public void Spawn()
+    {
+        StartCoroutine(OnSpawn());
+    }
+
+    /// <summary>
+    /// Coroutine that spawns a `unit` object every `spawnInterval` seconds using
+    /// its bucket until it empties, then it gets self destroyed.
+    /// </summary>
+    /// <returns>The IEnumerator for the coroutine.</returns>
+    private IEnumerator OnSpawn()
+    {
+        while (remaining-- != 0)
+        {
+            Instantiate(unit, transform.position, Quaternion.identity, transform);
+            yield return new WaitForSeconds(spawnInterval);
+        }
+
+        // Destroy the component to reduce memory usage as it has used
+        // all its slots. There is no problem with SpawnerTrigger as
+        // it gets the gameObject destroyed way before this happens.
+        Destroy(this);
+    }
+
+}
---
+++ Goblin Slayer/Assets/Scripts/SpawnerTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class SpawnerTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && collision.name == "Player")
+        {
+            // SpawnerTrigger is always a children of Spawner, therefore,
+            // a Spawner instance must be available in parent.
+            var spawner = GetComponentInParent<Spawner>();
+            if (spawner) spawner.Spawn();
+
+            // Once this trigger has been activated, it has no use, destroy
+            // the GameObject and all its components to free memory.
+            Destroy(gameObject);
+        }
+    }
+
+}
