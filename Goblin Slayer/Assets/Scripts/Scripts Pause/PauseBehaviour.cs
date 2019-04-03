4d472b1ec31415b4d915b4de388aa37970162b7f
Stiven <starias@ucm.es>
2019-04-03 16:34:54 +0200

    Arreglado el problema con el HUD y sus botones

--- Goblin Slayer/Assets/Scripts/Scripts Pause/PauseBehaviour.cs
+++ Goblin Slayer/Assets/Scripts/Scripts Pause/PauseBehaviour.cs
@@ -7,16 +7,25 @@ public class PauseBehaviour : MonoBehaviour
 {
     
     public GameObject pauseScreen, gameScreen;
+    public bool pauseActive;
+    public GameManager gamemanager;
+    public KeyCode exitKey;
+    public int scene;
+
     private void Start()
     {
         DesactivePause();
     }
-
+    private void Update()
+    {
+        if (pauseActive && Input.GetKeyDown(exitKey)) gamemanager.ChangeScene(scene);
+    }
     public void ActivePause()
     {
         pauseScreen.SetActive(true);
         gameScreen.SetActive(false);
         Time.timeScale = 0f;
+        pauseActive = true;
     }
 
     public void DesactivePause()
@@ -24,5 +33,6 @@ public class PauseBehaviour : MonoBehaviour
         pauseScreen.SetActive(false);
         gameScreen.SetActive(true);
         Time.timeScale = 1f;
+        pauseActive = false;
     }
 }
