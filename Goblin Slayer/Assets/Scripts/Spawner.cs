9015656f1e27eae8880d0b46404081aa5c80902c
Kyra <kyradiscord@gmail.com>
2019-04-03 10:04:37 +0200

    feat: Spawner refit

--- Goblin Slayer/Assets/Scripts/Spawner.cs
+++ Goblin Slayer/Assets/Scripts/Spawner.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour {
 
-    /// <summary>
-    /// The GameObject prefab this Spawner should instantiate.
-    /// </summary>
-    public GameObject unit;
+    [System.Serializable]
+    public struct UnitType
+    {
+        public GameObject GameObject;
+        public uint Amount;
+    }
 
     /// <summary>
-    /// The bucket for this spawner, this equals to the amount
-    /// of `unit` clones this will create.
+    /// The list of GameObjects with their amounts this Spawner should instantiate. 
     /// </summary>
-    public short remaining = 20;
+    public List<UnitType> unitTypes;
 
     /// <summary>
     /// The time interval in seconds for the spawner, an higher
     /// amount means more time between spawns.
     /// </summary>
-    public float spawnInterval = 3.5f;
+    public float spawnInterval = 0.5f;
     
     /// <summary>
     /// Start the spawning coroutine.
@@ -36,10 +37,13 @@ public class Spawner : MonoBehaviour {
     /// <returns>The IEnumerator for the coroutine.</returns>
     private IEnumerator OnSpawn()
     {
-        while (remaining-- != 0)
+        foreach (UnitType unitType in unitTypes)
         {
-            Instantiate(unit, transform.position, Quaternion.identity, transform);
-            yield return new WaitForSeconds(spawnInterval);
+            for (int i = 0; i < unitType.Amount; i++)
+            {
+                Instantiate(unitType.GameObject, transform.position, Quaternion.identity, transform);
+                yield return new WaitForSeconds(spawnInterval);
+            }
         }
 
         // Set enabled to false to prevent further CPU usage.
