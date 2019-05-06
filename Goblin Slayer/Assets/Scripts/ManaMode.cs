13db781a4288c13c1b9adc078e558882fba6182f
AnassCarreau <anasca01@ucm.es>
2019-05-06 13:09:18 +0200

    ModeMana
    Controla el mana segun la vida que tiene el player

---
+++ Goblin Slayer/Assets/Scripts/ManaMode.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaMode : MonoBehaviour
+{
+    public ManaState ModeState;
+
+
+    private Mana mn;
+    private Health health;
+    public float autoManaNormal;
+    public float autoManaBattle;
+    public float autoManaCritic;
+
+    void Start()
+    {
+        mn = GetComponent<Mana>();
+        health = GetComponent<Health>();
+    }
+    void Update()
+    {
+        switch (ModeState)
+        {
+            case ManaState.Normal:
+                mn.autoManaRegenRate = autoManaNormal;
+                if (health.GetHP() < health.maxHealth * 3 / 4)
+
+                { ModeState = ManaState.Battle; }
+
+
+                break;
+            case ManaState.Battle:
+                mn.autoManaRegenRate = autoManaBattle;
+
+                if (health.GetHP() < health.maxHealth * 1 / 4)
+                {
+                    ModeState = ManaState.Critic;
+                }
+                else if (health.GetHP() >= health.maxHealth * 3 / 4) ModeState = ManaState.Normal;
+
+                break;
+            case ManaState.Critic:
+                mn.autoManaRegenRate = autoManaCritic;
+                if (health.GetHP() > health.maxHealth * 1 / 4)
+                {
+
+                    ModeState = ManaState.Battle;
+                }
+
+                break;
+        }
+    }
+
+    public enum ManaState
+    {
+        Normal,
+        Battle,
+        Critic
+    }
+}
