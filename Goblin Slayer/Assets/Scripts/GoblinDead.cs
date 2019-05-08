cd4fb403157e0a3d53b21420107edf4637a5d85d
Amaro Blest <ablest@ucm.es>
2019-05-08 12:32:55 +0200

    Fixed dead of all goblins and OnGround bug

--- Goblin Slayer/Assets/Scripts/GoblinDead.cs
+++ Goblin Slayer/Assets/Scripts/GoblinDead.cs
@@ -4,38 +4,27 @@ using UnityEngine;
 
 public class GoblinDead : MonoBehaviour, IDead
 {
-    private Animator animator;
     private Rage playerRage;
     private Rigidbody2D rb;
-    private Collider2D collider2D;
     public int rageAmount = 2;
     public RageDoll rageDoll;
     private PlayerBaseSounds sounds;
-    private bool goblinDead = false;
+    private Transform rageDollPool;
 
     private void Start()
     {
+        rageDollPool = GameObject.Find(("RageDollPool")).transform;
         sounds = GetComponentInChildren<PlayerBaseSounds>();
-        animator = GetComponentInChildren<Animator>();
         playerRage = GameObject.Find("Player").GetComponent<Rage>();
     }
 
     void IDead.OnDead()
     {
-        goblinDead = true;
-
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (goblinDead && collision.GetContact(0).normal == Vector2.up)
-        {
-            sounds.PlayEffect(sounds.dead);
-            GetComponent<GoblinState>().GoblinIsDead();
-            GameManager.instancia.AddEnemy();
-            playerRage.AddRage(rageAmount);
-            Instantiate(rageDoll, transform.position, transform.rotation);
-            Destroy(gameObject);
-        }
+        sounds.PlayEffect(sounds.dead);
+        GetComponent<GoblinState>().GoblinIsDead();
+        GameManager.instancia.AddEnemy();
+        playerRage.AddRage(rageAmount);
+        Instantiate(rageDoll, transform.position, transform.rotation,rageDollPool);
+        Destroy(gameObject);
     }
 }
--- Goblin Slayer/Assets/Scripts/RageDoll.cs
+++ Goblin Slayer/Assets/Scripts/RageDoll.cs
@@ -1,22 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RageDoll : MonoBehaviour
 {
-    private Animator animator;
-
-    public void Die()
-    {
-        //Destroy(gameObject);
-    }
+    private Rigidbody2D rb;
+    
 
 	void Start ()
     {
+        rb = GetComponent<Rigidbody2D>();
         if (CompareTag("Player"))
         {
             GameManager.instancia.ResetScene(4);
         }
 
 	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Blocks"))
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+    }
 }
