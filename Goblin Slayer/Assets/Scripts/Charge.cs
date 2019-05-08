2ff12241fe60a31a26f5f2756a17b02f2d383994
Kyra <kyradiscord@gmail.com>
2019-05-08 12:33:14 +0200

    misc: Added more things and first functional Charge AI

--- Goblin Slayer/Assets/Scripts/Charge.cs
+++ Goblin Slayer/Assets/Scripts/Charge.cs
@@ -1,15 +1,63 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
-public class Charge : MonoBehaviour {
+public class Charge : MonoBehaviour
+{
+    public int Damage = 50;
+    public int CooldownTime = 1500;
+    public int Velocity = 5;
 
-	private enum Direction
-	{
-		Left,
-		Right
-	}
+    private enum Direction
+    {
+        Left,
+        Right
+    }
 
-	public void ChargeAt(Transform other)
-	{
-		
-	}
+    private Rigidbody2D rb;
+    public bool charging;
+    public float nextAvailableChargeTime;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        nextAvailableChargeTime = Time.time;
+    }
+
+    public void ChargeAt(Transform other)
+    {
+        Debug.Log("Trying to charge...");
+        if (charging || Time.time < nextAvailableChargeTime) return;
+        Debug.Log("Charging");
+        StartCoroutine(ChargeDirection(transform.position.x > other.transform.position.x
+            ? Direction.Left
+            : Direction.Right));
+    }
+
+    private IEnumerator ChargeDirection(Direction direction)
+    {
+        charging = true;
+        var vector = direction == Direction.Left ? Vector2.left : Vector2.right;
+        while (charging)
+        {
+            rb.AddForce(vector, ForceMode2D.Impulse);
+            yield return null;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Touched something... am I charging?");
+        // If it is not charging, ignore
+        if (!charging) return;
+        Debug.Log(string.Format("Touched something, with the tag of {0}", other.gameObject.tag));
+
+        // If it collided with an attackable, deal damage
+        var attackable = other.gameObject.GetComponent<Attackable>();
+        if (attackable)
+        {
+            attackable.OnAttack(Damage);
+            charging = false;
+            nextAvailableChargeTime = Time.time + CooldownTime;
+        }
+    }
 }
--- Goblin Slayer/Assets/Scripts/IA/Charge_AI.cs
+++ Goblin Slayer/Assets/Scripts/IA/Charge_AI.cs
@@ -1,44 +1,34 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(RangeObserver_AI))]
-[RequireComponent(typeof(Follower_AI))]
 [RequireComponent(typeof(Charge))]
 [RequireComponent(typeof(Target_AI))]
 public class Charge_AI : Brain_AI
 {
     private RangeObserver_AI rangeObserverAi;
-    private Follower_AI followerAi;
     private Charge charge;
     private Target_AI targetAi;
 
     private void Start()
     {
         rangeObserverAi = GetComponent<RangeObserver_AI>();
-        followerAi = GetComponent<Follower_AI>();
         charge = GetComponent<Charge>();
         targetAi = GetComponent<Target_AI>();
+        RegisterCallbacks();
     }
 
     protected override void RegisterCallbacks()
     {
-        rangeObserverAi.AddInRangeCallback(OnRange);
         rangeObserverAi.AddTooFarCallback(OnTooFar);
     }
 
     protected override void UnRegisterCallbacks()
     {
-        rangeObserverAi.RemoveInRangeCallback(OnRange);
         rangeObserverAi.RemoveTooFarCallback(OnTooFar);
-    }
-
-    private void OnRange()
-    {
-        followerAi.StopFollowing();
-        charge.ChargeAt(targetAi.GetTarget());
     }
 
     private void OnTooFar()
     {
-        followerAi.Follow();
+        charge.ChargeAt(targetAi.GetTarget());
     }
 }
