e24f4903683a9a5b7d3abdbb720cb4d4a1d78c12
AnassCarreau <anasca01@ucm.es>
2019-05-12 15:18:01 +0200

    Revert "Revert "Merge branch 'ModeMana' into IncreaseAttack""
    This reverts commit cfc97f8f681a59b26eb1305bc93160ceefbdb9ec.

---
+++ Goblin Slayer/Assets/Scripts/ManaMode.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaMode : MonoBehaviour
+{
+    public ManaState ModeState;
+    private PlayerAttackManager pl;
+    public enum ManaState { Normal, Battle, Critic }
+    private Mana mn;
+    private Health health;
+    public float autoManaNormal;
+    public float autoManaBattle;
+    public float autoManaCritic;
+    public float IncrementoModoGuerrero;
+    void Start()
+    {
+        pl = GetComponent<PlayerAttackManager>();
+        mn = GetComponent<Mana>();
+        health = GetComponent<Health>();
+    }
+
+    public void ChangueRegenMana()
+    {
+        switch (ModeState)
+        {
+            case ManaState.Normal:
+                mn.autoManaRegenRate = autoManaNormal;
+                if (health.GetHP() < health.maxHealth * 3.0f / 4.0f)
+
+                { ModeState = ManaState.Battle; }
+                break;
+            case ManaState.Battle:
+                mn.autoManaRegenRate = autoManaBattle;
+
+                if (health.GetHP() < health.maxHealth * 1.0f / 4.0f)
+                {
+                    ModeState = ManaState.Critic;
+                }
+                else if (health.GetHP() >= health.maxHealth * 3.0f / 4.0f) ModeState = ManaState.Normal;
+                break;
+            case ManaState.Critic:
+                mn.autoManaRegenRate = autoManaCritic;
+                if (health.GetHP() > health.maxHealth * 1 / 4)
+                {
+
+                    ModeState = ManaState.Battle;
+                }
+                break;
+        }
+        if (pl.GetMode() == 0)
+        {
+            mn.autoManaRegenRate *= IncrementoModoGuerrero;
+        }
+    }
+}
+
+    
+
