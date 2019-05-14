9ee104409e158ed10c734462ff1e932124a00a0a
OskarFreestyle <47836840+OskarFreestyle@users.noreply.github.com>
2019-05-14 22:03:44 +0200

    recursos añadidos (no funcional)

--- Goblin Slayer/Assets/Scripts/CameraFollowTarget.cs
+++ Goblin Slayer/Assets/Scripts/CameraFollowTarget.cs
@@ -16,5 +16,6 @@ public class CameraFollowTarget : MonoBehaviour
     void Update ()
     {
         transform.position = new Vector3(target.position.x + difference, target.position.y+difference, distance);
+
 	}
 }
---
+++ Goblin Slayer/Assets/Scripts/ParallaxBG.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParallaxBG : MonoBehaviour
+{
+
+    public void Start()
+    {
+        RawImage bg = GetComponent<RawImage>();
+    }
+
+
+    public void Update()
+    {
+        
+    }
+}
